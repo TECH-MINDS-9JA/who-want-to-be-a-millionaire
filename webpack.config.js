a1c0710586527f16a7c43d9e03f4cf196b949880
webagent-dev <webagent24@gmail.com>
2022-06-16 00:38:41 +0100

    setup complete and working

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const patch = require('path')
-
+const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
-    entry: "index.js",
+    entry: path.join(__dirname, "src", "index.js"),
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
     target: 'web',
@@ -16,15 +16,27 @@ module.exports = {
         liveReload: true
     },
     resolve: {
-        extensions: ['.js','jsx','.json'],
+        extensions: ['.js','.jsx','.json'],
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: "babel-loader"
+                use:
+                {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }    
             }
         ]
-    }
+    }, 
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: "who-want-to-be-a-millionaira",
+            template: path.join(__dirname, "src", "index.html")
+        })
+    ]
 }
